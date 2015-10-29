52e5c786bb697570091927e3f3fb84ea229c617e
05K4R1N <oskr.cbba2010@gmail.com>
2015-10-29 16:06:41 -0400

    primeros pasos

---
+++ controlador/usuario.controlador.php
@@ -0,0 +1,14 @@
+<?php
+
+	class UsuarioControlador(){
+
+		public function __CONSTRUCT(){
+
+		}
+	}
+	publuc function Index(){
+		require_once 'vista/header.html';
+		require_once 'vista/usuario/bienvenida';
+		require_once 'vista/footer.html';
+	}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php echo "una prueba"?>
+<?php
+	$controlador="usuario";
+	if(!isset($_REQUEST["c"])){
+		require_once 'controlador/$controlador.controlador.php';
+		$controlador=ucwords($controlador).'Controlador';
+		$controlador=new $controlador;
+		$controlador->Index();
+	}
+?>
