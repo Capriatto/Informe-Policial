eb17ef0570d621b7add2a2a6a6b20fb0ccb43af6
05K4R1N <oskr.cbba2010@gmail.com>
2015-11-18 08:54:07 -0400

    añadiendo banner

--- vista/usuario/bienvenida.php
+++ vista/usuario/bienvenida.php
@@ -1,5 +1,40 @@
 	<section class="main-container">
-		
+		<section class="portada	visible-lg visible-md">	
+			<div class="container">
+				<div class="col-md-12">
+					<div id="carousel-1" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-1" data-slide-to="1"></li>
+							<li data-target="#carousel-1" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="images/policiasEnFila.jpg" class="imagenPortada">
+								<div class="carousel-caption">
+									<h2>CENTRO POLICIAL</h2>
+									<p>Al servicio de la población</p>
+								</div>
+							</div>
+							<div class="item">
+								<img src="images/policiasuniformados.jpg" class="imagenPortada">
+								<div class="carousel-caption">
+									<h2>CENTRO POLICIAL</h2>
+									<p>Preparados para cualquier emergencia o peligro a enfnrentar</p>
+								</div>
+							</div>
+							<div class="item">
+								<img src="images/uniformados.jpg" class="imagenPortada">
+								<div class="carousel-caption">
+									<h3>CENTRO POLICIAL</h3>
+									<p>Atencion a la ciudadania brindando seguridad ciudadana</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</section>
 	<script src="assets/js/jquery.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
