4d72959790cd82db312c8cdb33f6f213881b0578
05K4R1N <oskr.cbba2010@gmail.com>
2015-11-22 11:17:31 -0400

    descripcion e informacion

--- vista/usuario/bienvenida.php
+++ vista/usuario/bienvenida.php
@@ -35,16 +35,57 @@
 				</div>
 			</div>
 		</section>
-		<aside class="col-lg-4">
-			<h3 class="TituloMenu">INFORMACION</h3>
-			<div class="list-group menuInfo">
-				<a href="#" class="list-group-item">Noticas</a>
-				<a href="#" class="list-group-item">Direccion de Estacion Policial</a>
-				<a href="#" class="list-group-item">Lineas de Emergencia</a>
-				<a href="#" class="list-group-item">Acerca De</a>
-				<a href="#" class="list-group-item">Personal Administrativo</a>
-			</div>
-		</aside>
+		<section class="principal">
+			<aside class="col-lg-4 hidden-xs hidden-sm">
+				<h3 class="tituloPrincipal">INFORMACION</h3>
+				<div class="list-group menuInfo">
+					<a href="#" class="list-group-item">Noticas</a>
+					<a href="#" class="list-group-item">Direccion de Estacion Policial</a>
+					<a href="#" class="list-group-item">Lineas de Emergencia</a>
+					<a href="#" class="list-group-item">Acerca De</a>
+					<a href="#" class="list-group-item">Personal Administrativo</a>
+				</div>
+			</aside>
+			<section class="col-lg-8 col-xs-12 col-sm-12">
+				<h3 class="tituloPrincipal">DESCRIPCION</h3>
+				<div class="descripcion">
+					<div class="imagenDescripcion">
+						<img class="iconoHome" src="images/icono policia.png" align="left">
+					</div>
+					<div class="parrafo">
+						<p>
+						Somos una institución dedicada al mantenimiento del orden público, seguridad ciudadana
+						mediante el monopolio de la fuerza y cumplimiento de las leyes. Estamos sometida  las órdenes del Estado.<br>
+						</p>
+					 </div>
+				</div>
+				<div class="descripcion">
+					<div class="imagenDescripcion">
+						<img class="iconoHome" src="images/icono asalto.png" align="left">
+					</div>
+					<div class="parrafo">
+						<p>
+						Nuestra obligación es disuadir  e investigar crímenes en contra de las personas o que afecten 
+						el orden público, así comoel arresto de sospechosos, e informe a las autoridades competentes.
+						<br>Así como también  reportar ofensas menores  por medio  de citaciones que suelen terminar en un
+						pago de fianza, usualmente por violaciones  a las leyes del tránsito vehicular.
+						</p>	
+					</div>
+				</div>
+				<div class="descripcion">
+					<div class="imagenDescripcion">
+						<img class="iconoHome" src="images/icono patrulla.png" align="left">
+					</div>
+					<div class="parrafo">
+						<p>
+						Realizamos patrullaje en donde existe mayor delincuencia, tenemos varios <a class="btn btn-primary btn-xs" href="#">líneas telefónicas</a> disponibles 
+						para atender cualquier emergencia y poder asistirlo con el envío de una patrulla y por esa razón el objetivo ante esto es 
+						poder preservar la armonía social con actividades que se encuentren dentro del orden.
+						</p>
+					</div>
+				</div>
+			</section>
+		</section>
 	</section>
 	<script src="assets/js/jquery.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
